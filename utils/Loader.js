44d3616d3f1642769404beeda44ff8cb337cc309
Sven Outters <sven.outters@hotmail.fr>
2026-03-13 14:04:09 +0100

    Simple loading screen

--- utils/Loader.js
+++ utils/Loader.js
@@ -2,8 +2,26 @@ import * as THREE from "three";
 
 export class Loader {
   constructor() {
-    this.textureLoader = new THREE.TextureLoader();
-    this.cubeTextureLoader = new THREE.CubeTextureLoader();
+    this.manager = new THREE.LoadingManager();
+
+    this.textureLoader = new THREE.TextureLoader(this.manager);
+    this.cubeTextureLoader = new THREE.CubeTextureLoader(this.manager);
+
+    const progressBar = document.getElementById("loading-progress");
+    const screen = document.getElementById("loading-screen");
+
+    this.manager.onProgress = (url, loaded, total) => {
+      const progress = (loaded / total) * 100;
+      progressBar.style.width = progress + "%";
+    };
+
+    this.manager.onLoad = () => {
+      screen.style.opacity = "0";
+
+      setTimeout(() => {
+        screen.style.display = "none";
+      }, 500);
+    };
   }
 
   loadTexture(path) {
